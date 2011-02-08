3040f79c43d806f6d0308c221b87cff08bca5ecc
unknown <DiaAWAY@.(none)>
2011-02-08 09:41:34 +0100

    Initial commit

---
+++ src/main/java/com/bukkit/diaaway/homer/HPlayerListener.java
@@ -0,0 +1,26 @@
+package com.bukkit.diaaway.homer;
+
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerInventoryEvent;
+import org.bukkit.event.player.PlayerItemEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author DiaAWAY
+ */
+public class HPlayerListener extends PlayerListener {
+
+    private final HomerPlugin plugin;
+
+    public HPlayerListener(HomerPlugin instance) {
+        plugin = instance;
+    }
+}

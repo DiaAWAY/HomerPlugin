85fc7b22daabc998fe1a493571a2b10905190183
DiaAWAY <DiaAWAY@.(none)>
2011-02-10 01:36:07 +0100

    renamed namespace, wasn't actually going public at first, but since it is public now...

---
+++ src/main/java/com/diaaway/homer/HBlockListener.java
@@ -0,0 +1,77 @@
+package com.diaaway.homer;
+
+import com.nijikokun.bukkit.iProperty;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+/**
+ * HomerPlugin block listener
+ * @author DiaAWAY
+ */
+public class HBlockListener extends BlockListener {
+
+    private final HomerPlugin plugin;
+    private iProperty homes = new iProperty("homerPlugin.properties");
+
+    public HBlockListener(final HomerPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        if (isTrespassing(player)) {
+            event.setCancelled(true);
+            player.sendMessage("This land belongs to someone, you can't place blocks here!");
+        }
+    }
+
+    private boolean isTrespassing(Player player) {
+        boolean passX = true;
+        boolean passY = true;
+        boolean passZ = true;
+        Iterator it = null;
+        try {
+            it = homes.returnMap().keySet().iterator();
+        } catch (Exception ex) {
+            Logger.getLogger(HBlockListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (it != null) {
+            Location loc = player.getLocation();
+            while (it.hasNext()) {
+                System.out.println("a: " + it.next());
+
+            }
+//            int mod = 1;
+//            for (int i = 0; i < values.length; i++) {
+//                System.out.println("values: " + values[i]);
+//                // magic numbers ahoy! ;D
+//                // check x
+//                if (values[(i * mod)] + values[(i * mod) + 3] >= loc.getX()
+//                        && values[(i * mod)] - values[(i * mod) + 3] <= loc.getX()) {
+//                    passX = false;
+//                }
+//                // check y
+//                if (values[(i * mod) + 1] + values[(i * mod) + 3] >= loc.getX()
+//                        && values[(i * mod)] - values[(i * mod) + 3] <= loc.getX()) {
+//                    passY = false;
+//                }
+//                // check z
+//                if (values[(i * mod) + 2] + values[(i * mod) + 3] >= loc.getX()
+//                        && values[(i * mod)] - values[(i * mod) + 3] <= loc.getX()) {
+//                    passZ = false;
+//                }
+//                if (i % 4 == 0) {
+//                    mod += 1;
+//                }
+//            }
+        }
+        return (passX && passY && passZ);
+    }
+}
---
+++ src/main/java/com/diaaway/homer/HomerPlugin.java
@@ -0,0 +1,153 @@
+package com.diaaway.homer;
+
+import java.io.File;
+import java.util.HashMap;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+import com.nijikokun.bukkit.iProperty;
+import org.bukkit.Location;
+
+/**
+ * Plugin for bukkit that enables users to:
+ * * save a home location and teleport to it
+ * * protect the area round its home location
+ *
+ * @author DiaAWAY
+ */
+public class HomerPlugin extends JavaPlugin {
+
+    private final HPlayerListener playerListener = new HPlayerListener(this);
+    private final HBlockListener blockListener = new HBlockListener(this);
+    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
+    private iProperty homes = new iProperty("homerPlugin.properties");
+//    private boolean teleport = true; // wether teleport is enabled or not
+    private double homeSize = 10; // initial homeSize of the home, for block destruction/placement purposes
+
+    public HomerPlugin(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
+        super(pluginLoader, instance, desc, folder, plugin, cLoader);
+        // TODO: Place any custom initialisation code here
+
+        // NOTE: Event registration should be done in onEnable not here as all events are unregistered when a plugin is disabled
+    }
+
+    public void onDisable() {
+        // TODO: Place any custom disable code here
+
+        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        System.out.println("Goodbye world!");
+    }
+
+    public void onEnable() {
+        // TODO: Place any custom enable code here including the registration of any events
+
+        // Register our events
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_QUIT, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_COMMAND, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_MOVE, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_PHYSICS, blockListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.BLOCK_CANBUILD, blockListener, Priority.Low, this);
+        pm.registerEvent(Event.Type.BLOCK_PLACED, blockListener, Priority.Low, this);
+
+        // look for the property file, if it is not there create it, if it is; load it.
+
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+    }
+
+    public boolean isDebugging(final Player player) {
+        if (debugees.containsKey(player)) {
+            return debugees.get(player);
+        } else {
+            return false;
+        }
+    }
+
+    public void setDebugging(final Player player, final boolean value) {
+        debugees.put(player, value);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        String[] trimmedArgs = args;
+        String cmdName = cmd.getName().toLowerCase();
+        if (cmdName.equals("home")) {
+            return performHome(sender, trimmedArgs);
+        }
+        if (cmdName.equals("sethome")) {
+            return performSetHome(sender, trimmedArgs);
+        }
+        return false;
+    }
+    
+    /**
+     *
+     * @param sender
+     * @param split
+     * @return
+     */
+    private boolean performSetHome(CommandSender sender, String[] split) {
+        if (split.length != 0) return false; // this command is a oneliner, assume they need command description :)
+        Player player = (Player) sender;
+        homes.setDouble(player.getName() + ".x", player.getLocation().getX());
+        homes.setDouble(player.getName() + ".y", player.getLocation().getY());
+        homes.setDouble(player.getName() + ".z", player.getLocation().getZ());
+        homes.setDouble(player.getName() + ".homeSize", getHomeSize());
+        player.sendMessage("Home has been set!");
+        return true;
+    }
+
+    /**
+     *
+     * @param sender
+     * @param split
+     * @return
+     */
+    private boolean performHome(CommandSender sender, String[] split) {
+        Player player = (Player) sender;
+        World world = sender instanceof Player ? ((Player) sender).getWorld() : getServer().getWorlds().get(0);
+        
+        if (getServer().getPlayer(player.getName()) == null) {
+            return false;
+        }
+        if (split.length != 0) return false; // this command is a oneliner, assume they need command description :)
+        if (homes.keyExists(player.getName() + ".x")) { // check if the player's home is set. 
+
+            player.teleportTo(new Location(world,
+                    homes.getDouble(player.getName() + ".x"),
+                    homes.getDouble(player.getName() + ".y"),
+                    homes.getDouble(player.getName() + ".z")));
+            player.sendMessage("Zwosh!");
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return the homeSize of the home
+     */
+    protected double getHomeSize() {
+        return homeSize;
+    }
+
+    /**
+     * @param homeSize the homeSize to set the home to
+     */
+    protected void setHomeSize(double size) {
+        this.homeSize = size;
+    }
+}
